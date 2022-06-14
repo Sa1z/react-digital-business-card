fa44fe6ccbd1b2dffc5866d880fff335ff536624
Dennis Diaz <Diaz3737@gmail.com>
2022-06-14 14:00:55 -0400

    Initial commit.

---
+++ src/Container.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Container() {
+    return (
+      <div className="Container">
+        <img className="headshot" src="https://c.pxhere.com/photos/31/11/dog_nose_snout_animal_head_eyes_pet_close-1391282.jpg!d" alt="dog" />
+        <h1 className="title-name">test</h1>
+      </div>
+    )
+  }
