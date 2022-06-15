4b63079c88cc3f4711c74cb6c77c41e8c8cf24ed
Dennis Diaz <Diaz3737@gmail.com>
2022-06-15 18:00:53 -0400

    Done. Real info and links needed.

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,4 +1,9 @@
-
+import MailIcon from './images/Mail.png'
+import LinkedInIcon from './images/Vector.png'
+import TwitterIcon from './images/Twitter-Icon.png'
+import FacebookIcon from './images/Facebook-Icon.png'
+import InstagramIcon from './images/Instagram-Icon.png'
+import GitHubIcon from './images/GitHub-Icon.png'
 
 export default function Container() {
     return (
@@ -8,8 +13,8 @@ export default function Container() {
         <h2 className="title-desc">Web Developer</h2>
         <a href="https://sa1z.github.io/personal-site/" className="site-link">Dennis's Site</a>
         <div className="btn">
-          <button className="email"><img src="../images/Mail.png" alt="" /> Email</button>
-          <button className="linkedin"><img src="../images/Vector.png" alt="" /> LinkedIn</button>
+          <button className="email"><img src={MailIcon} alt="" /> Email</button>
+          <button className="linkedin"><img src={LinkedInIcon} alt="" /> LinkedIn</button>
         </div>
         <div className="about">
           <h2>About</h2>
@@ -20,10 +25,10 @@ export default function Container() {
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam mollitia dolorem reiciendis laudantium tempore atque earum nulla nostrum quae fugit? Similique dolorum fuga exercitationem perferendis expedita, omnis labore quis facere?</p>
         </div>
         <div className="social-links">
-          <a href="https://twitter.com/"><img src="../images/Twitter-Icon.png" alt="Twitter" /></a>
-          <a href="#"><img src="../images/Facebook-Icon.png" alt="Facebook" /></a>
-          <a href="#"><img src="../images/Instagram-Icon.png" alt="Instagram" /></a>
-          <a href="#"><img src="../images/GitHub-Icon.png" alt="Github" /></a>
+          <a href="#" target="_blank"><img src={TwitterIcon} alt="Twitter" /></a>
+          <a href="#" target="_blank"><img src={FacebookIcon} alt="Facebook" /></a>
+          <a href="#" target="_blank"><img src={InstagramIcon} alt="Instagram" /></a>
+          <a href="#" target="_blank"><img src={GitHubIcon} alt="Github" /></a>
         </div>
       </div>
     )
